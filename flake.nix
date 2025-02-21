e22dd9395e2253963a62d13892f92c305857f4b0
Permafrozen <permafrozen@disroot.org>
2025-02-21 17:24:19 +0100

    added flake.nix for dev env

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Aglet is pretty Sigma.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ags, }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system} = {
+        default = ags.lib.bundle {
+          inherit pkgs;
+          src = ./src;
+          name = "aglet";
+          entry = "app.ts";
+          gtk4 = true;
+
+          # additional libraries and executables to add to gjs' runtime
+          extraPackages = [
+            # ags.packages.${system}.battery
+            # pkgs.fzf
+          ];
+        };
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            # includes astal3 astal4 astal-io by default
+            (ags.packages.${system}.default.override {
+              extraPackages = [
+                # cherry pick packages
+              ];
+            })
+          ];
+        };
+      };
+    };
+}

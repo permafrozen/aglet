098b53aab457624326410e080455a93021748289
Permafrozen <m8tt8o@gmail.com>
2025-03-13 07:59:40 +0100

    updated src/widget.tsx with astal-battery usage and modified default.scss

--- src/statusbar/widget.tsx
+++ src/statusbar/widget.tsx
@@ -1,11 +1,7 @@
 import { App, Astal, Gtk, Gdk } from "astal/gtk4";
-import { Variable } from "astal";
+import { bind, Variable } from "astal";
 import GLib from "gi://GLib?version=2.0";
-
-const time = Variable("").poll(
-    1000,
-    () => GLib.DateTime.new_now_local().format("%H:%M")!,
-);
+import Battery from "gi://AstalBattery";
 
 export default function Statusbar(gdkmonitor: Gdk.Monitor) {
     const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor;
@@ -21,13 +17,38 @@ export default function Statusbar(gdkmonitor: Gdk.Monitor) {
         >
             <centerbox cssName="centerbox">
                 <box />
-                <menubutton hexpand halign={Gtk.Align.CENTER}>
-                    <label label={time()} />
-                    <popover>
-                        <Gtk.Calendar />
-                    </popover>
-                </menubutton>
+                <Time />
+                <Power />
             </centerbox>
         </window>
+    );
+}
+
+function Time() {
+    const time = Variable("").poll(
+        1000,
+        () => GLib.DateTime.new_now_local().format("%H:%M")!,
+    );
+
+    return (
+        <menubutton hexpand halign={Gtk.Align.CENTER}>
+            <label label={time()} />
+            <popover cssName="calender">
+                <Gtk.Calendar />
+            </popover>
+        </menubutton>
+    );
+}
+
+function Power() {
+    const battery = Battery.get_default();
+    return (
+        <menubutton hexpand halign={Gtk.Align.END}>
+            <label
+                label={bind(battery, "percentage").as(
+                    (p) => `${Math.floor(p * 100)} %`,
+                )}
+            />
+        </menubutton>
     );
 }
